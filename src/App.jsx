e09ac217b5d95f42151a38d53908481ad6397b6c
jyunya-takahashi <67084292+jyunya-takahashi@users.noreply.github.com>
2021-08-02 22:45:05 +0900

    追加ボタンの処理を追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,30 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  // 入力値のstate化
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState(["aaaa", "iiiii"]);
   const [completeTodos, setCompleteTodos] = useState(["cccc"]);
+
+  // 入力値が変更になった場合の取得しstateを変更
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return; // 入力がない場合、処理をスキップ
+    const newTodos = [...incompleteTodos, todoText]; // 下の配列＋新しいtextを結合
+    setIncompleteTodos(newTodos); // stateの値を更新
+    setTodoText(""); // 入力欄を空白にセット
+  };
 
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
 
       <div className="incomplete-area">
